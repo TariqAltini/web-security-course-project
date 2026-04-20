3d665481d65df96bb22158251e14595f6732ad34
Tariq Altini <126665276+TariqAltini@users.noreply.github.com>
2026-04-20 09:44:38 +0300

    Setup applications

--- config/urls.py
+++ config/urls.py
@@ -19,4 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('secure/', include('apps.secure.urls')),
+    path('vulnerable/', include('apps.vulnerable.urls')),
 ]
