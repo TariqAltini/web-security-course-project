b7872f060cd7e526980e582b0e6f8f944e92ebf1
Tariq Altini <126665276+TariqAltini@users.noreply.github.com>
2026-05-03 19:24:23 +0300

    Implement login and logout views

--- apps/secure/urls.py
+++ apps/secure/urls.py
@@ -8,4 +8,6 @@ app_name = "secure"
 urlpatterns = [
     #path(...)
     path("", views.index, name="index"),
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
 ]
--- apps/secure/views.py
+++ apps/secure/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
 from django.http import HttpRequest, HttpResponse
 
@@ -10,3 +10,22 @@ def index(request: HttpRequest):
     products = Product.objects.all()
     context = {"products": products}
     return render(request, "secure/index.html", context=context)
+
+def login_view(request: HttpRequest):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect("index")
+        else:
+            return render(request, "secure/login.html", {"error": "Invalid credentials"})
+        
+    return render(request, "secure/login.html")
+
+def logout_view(request: HttpRequest):
+    logout(request)
+    redirect("index")
